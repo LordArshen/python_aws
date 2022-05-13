2a8d781bf5f1e4294c81607ada1876fb873feb00
LordArshen <ec2-user@ip-10-16-10-205.us-west-2.compute.internal>
2022-05-13 14:47:12 +0000

    first commit cloud9

---
+++ hello-world.py
@@ -0,0 +1,29 @@
+"""
+Your module description
+"""
+print("hello, world")
+
+myValue = 1 
+print( myValue )
+print( type(myValue) )
+
+print(f"${myValue} is of data type ${type(myValue) } ")
+
+myValue = 3.14
+
+print( myValue )
+print( type(myValue) )
+
+print(f"${myValue} is of data type ${type(myValue) } ")
+
+myValue = 5j 
+print( myValue )
+print( type(myValue) )
+
+print(f"${myValue} is of data type ${type(myValue) } ")
+
+myValue = True 
+print( myValue )
+print( type(myValue) )
+
+print(f"{myValue} is of data type {type(myValue) } ")
---
+++ listTupleDictionaries.py
@@ -0,0 +1,31 @@
+myFruitList = ["apple", "banana", "cherry"]
+print(myFruitList)
+print(type(myFruitList))
+
+print(myFruitList[0])
+
+myFruitList[2] = "orange"
+
+print(myFruitList)
+
+myFinalAnswerTuple = ("apple", "banana", "pineapple")
+print(myFinalAnswerTuple)
+print(type(myFinalAnswerTuple))
+
+print(myFinalAnswerTuple[0])
+
+myFavoriteFruitDictionary = {
+  "Akua" : "apple",
+  "Saanvi" : "banana",
+  "Paulo" : "pineapple"
+}
+
+myFavoriteFruitDictionary = {
+  "Akua" : "apple",
+  "Saanvi" : "banana",
+  "Paulo" : "pineapple"
+}
+
+print(type(myFavoriteFruitDictionary))
+
+print(myFavoriteFruitDictionary["Akua"])
