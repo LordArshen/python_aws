47853a45f63ec2b4b3505faf917febba06ea766c
LordArshen <m.r.barrera@hotmail.com>
2022-05-13 15:58:28 -0500

    structure folders-insuline lab

---
+++ lab-8/analyze-insulin.py
@@ -0,0 +1,36 @@
+import os
+
+
+#open file in read mode
+file = open("./lab-8/preproinsulin-seq.txt", "r")
+
+#read the content of file
+data = file.read()
+out = open("./lab-8/preproinsulin-seq-clean.txt", "wt")
+char_to_replace = {" " : "", "//": "", "1":'',"2":"","3":"","4":"","5":"","6":"","7":"","8":"","9":"","0":""}
+
+
+for key, value in char_to_replace.items():
+    # Replace key character with value character in string
+    
+    data=data.replace(key, value)
+
+    
+
+print(data)
+out.write(data)
+number_of_characters = len(data)
+""" number_of_characters_2 = len(out) """
+
+print('Number of characters in text file :', number_of_characters)
+
+
+
+out.close()
+
+
+
+
+
+
+
